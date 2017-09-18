7cacf7d3e164c279f67d1cbf4899ebd66c17119d
kckornelsen <kurt.kornelsen@gmail.com>
2017-09-18 12:52:18 -0400

    Update

--- runupdate.bat
+++ runupdate.bat
@@ -1,1 +1,4 @@
-call "update_AvData.bat" > outfile.txt
+@echo Started %date% %time% >> outfile2.txt
+call "update_AvData.bat" >> outfile2.txt
+@echo Completed: %date% %time%
+@echo &&
--- update_AvData.bat
+++ update_AvData.bat
@@ -1,7 +1,7 @@
-DEL outfile.txt
-(
+
+
 REM File to get new data for METARS and TAF and push it to the GitHub Pages
-echo %datetime%
+@echo %datetime% 
 
 python get_MET_TAF.py
 
@@ -9,5 +9,5 @@ git add index.html
 git commit -m "Update Index"
 git push
 
-)>outfile.txt
+
 
