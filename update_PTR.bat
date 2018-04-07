abd1490f07bf978151cb24abe9cef060786cabc2
kckornelsen <kurt.kornelsen@gmail.com>
2018-04-06 21:11:07 -0400

    Add PTR

---
+++ update_PTR.bat
@@ -0,0 +1,13 @@
+
+
+REM File to get new data for METARS and TAF and push it to the GitHub Pages
+@echo %datetime% 
+
+COPY "C:\Users\Home\Dropbox\SOGC Documents\Standards Documents\SCCFSPTR.htm" "C:\Users\Home\GitHub\SOGC_App\SCCFSPTR.html"
+
+git add SCCFSPTR.html
+git commit -m "Update PTR"
+git push
+
+
+
